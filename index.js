d645c0afd6007046102ec009254201f6a9701dd8
Gerardo Montero <gerardomontme@gmail.com>
2021-08-26 00:04:06 -0700

    async querys

--- index.js
+++ index.js
@@ -1,35 +1,12 @@
-const MongoClient = require('mongodb').MongoClient;
 const express = require('express');
 const basicAuth = require('express-basic-auth');
 const cors = require('cors');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
-const db = require('./CRUD.js');
+const mongo = require('./mongo')
 
-const url = "mongodb://localhost:27017/";
-
-function registerUser(cred) {
-    MongoClient.connect(url, function(err, db) {
-        if (err) console.log(err);
-        let dbo = db.db("UserDB");
-        dbo.collection("Users").insertOne(cred, function(err, res) {
-            if (err) console.log(err);
-            console.log("User Registered");
-            db.close();
-        });
-     }); 
- }
-
- function loginUser(cred) {
-    MongoClient.connect(url, function(err, db) {
-        if (err) console.log(err);
-        let dbo = db.db("UserDB");
-        dbo.collection("Users").findOne(cred, function(err, result) {
-            if (err) console.log(err);
-            db.close();
-        });
-     });
- }
+mongo.db.createNewCollection('Users')
+mongo.db.createNewCollection('Admin')
 
 const app = express();
 
@@ -47,69 +24,39 @@ app.use(session({
 
 app.use(express.json());
 
-// app.use(basicAuth({
-//     authorizer: (username, password) => {
-//         const userMatches = basicAuth.safeCompare(username, 'admin')
-//         const passwordMatches = basicAuth.safeCompare(password, 'supersecret')
-//         return userMatches & passwordMatches
-//     },
-//     unauthorizedResponse: (req) => {
-//         return `unauthorized. ip: ${req.ip}`
-//     }
-// }))
 
 app.post('/login', (req,res,next) => {
 
-    let cred = { email: req.body.email, password: req.body.password };
-    loginUser(cred)
-
-    req.session.user = cred;
-    console.log(req.session)
-    res.json('logged in')
-
-
-    // db.crud.inMemoryDatabase.filter(user => {
-    //     const userMatches = basicAuth.safeCompare(req.body.email, user.email)
-    //     const passwordMatches = basicAuth.safeCompare(req.body.password, user.password)
-    //     if (userMatches && passwordMatches){
-    //         req.session.user = user;
-    //         console.log(req.session)
-    //         res.json('logged in')
-    //     }
-    // })
+    async function login(){
+        let cred = { email: req.body.email, password: req.body.password };
+        let user = await mongo.db.loginUser(cred)
+        if (user){
+            req.session.user = cred;
+            console.log(req.session)
+            res.json('logged in')
+        } else {
+            res.json('Invalid credentials')
+        }
+    }
+    login()
 })
 
 app.post('/signup', (req,res,next) => {
-    let cred = { email: req.body.email, username:req.body.username, password: req.body.password };
-    registerUser(cred)
-    req.session.user = cred;
-    console.log(req.session)
-    res.json('signup successful')
-    
-    
-
-    // let match = db.crud.inMemoryDatabase.find(user => {
-    //     const userMatches = basicAuth.safeCompare(req.body.email, user.email)
-    //     if (userMatches){
-    //         return true
-    //     }
-    // })
-    // if (match){
-    //     res.json('Username already in use')
-    // } else {
-    //     const create = db.crud.Create(req.body.email,req.body.password)
-    //     let recordData = {username: req.body.username}
-    //     const update = db.crud.Update(req.body.email,recordData)
-    //     console.log(create,update);
-    //     if (create && update) {
-    //         db.crud.flushDB()
-    //         req.session.user = {email:req.body.email};
-    //         console.log(req.session)
-    //         res.json('signup successful')
-    //     } else {
-    //         res.json('signup failed')
-    //     }
-    // }
+    async function signup(){
+        let cred = { email: req.body.email, username:req.body.username, password: req.body.password };
+        if (await mongo.db.registerUser(cred)){
+            req.session.user = cred;
+            console.log(req.session)
+            res.json('signup successful')
+        } else {
+            res.json('Invalid credentials') 
+        }
+    }
+    if (req.body.password === req.body.password2){
+       signup() 
+    } else {
+        res.json("Passwords don't match") 
+    }
 })
 
 
--- mongo.js
+++ mongo.js
@@ -1,17 +1,55 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
+class db {
+    constructor(){
 
-function createNewCollection(collectionName) {
-   MongoClient.connect(url, function(err, db) {
-        if (err) console.log(err);
-        var dbo = db.db("UserDB");
-        dbo.createCollection(collectionName, function(err, res) {
-            if (err) console.log(err);
-            console.log("Collection created!");
-            db.close();
-        });
-    }); 
+    }
+    MongoClient = require('mongodb').MongoClient;
+    url = "mongodb://localhost:27017/";
+    createNewCollection(collectionName) {
+        this.MongoClient.connect(this.url, function(err, db) {
+                if (err) console.log(err);
+                var dbo = db.db("UserDB");
+                dbo.createCollection(collectionName, function(err, res) {
+                    if (err) console.log(err);
+                    console.log("Collection created!");
+                    db.close();
+                });
+        }); 
+    }
+
+    async registerUser(cred) {
+        let db = await this.MongoClient.connect(this.url)
+        let dbo = db.db("UserDB");
+
+        if (await dbo.collection("Users").findOne(cred)){
+            console.log('User already exists')
+        } else {
+            try {
+                let res = await dbo.collection("Users").insertOne(cred)
+                console.log("User Registered");
+                await db.close();
+                return res
+            } catch (err) {
+                console.log(err);
+                db.close(); 
+                return false
+            }
+        }
+
+    }
+
+    async loginUser(cred) {
+        let db = await this.MongoClient.connect(this.url)
+        let dbo = db.db("UserDB");
+        try {
+            let result = await dbo.collection("Users").findOne(cred);
+            await db.close;
+            return result;
+        } catch {
+            console.log(err);
+            db.close;
+            return false
+        }
+    }
 }
 
-createNewCollection('Users')
-createNewCollection('Admin')
+exports.db = new db();
